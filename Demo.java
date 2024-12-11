112b7a8ae6fc171a9adc869aafce705427b97426
Горошко Никита Ф. <fpm.goroshkoNF@bsu.by>
2024-12-11 14:10:49 +0300

    yes

---
+++ Demo.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+public class Demo {
+
+    public static void main(String[] args) {
+        downloader naiveDownloader = new downloader(new ThirdPartyYouTubeClass());
+        downloader smartDownloader = new downloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+
+    }
+
+    private static long test(downloader downloader) {
+        long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+        return estimatedTime;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.lang.reflect.Proxy;
+import  java.rmi.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
